2ce73c96de50d50152d70c49a106760451bb255e
shienka07 <shienka07.ko@gmail.com>
2023-07-13 14:29:19 +0900

    배열의 유사도

---
+++ 2023/2023_07/230713.java
@@ -0,0 +1,31 @@
+/*
+두 배열이 얼마나 유사한지 확인해보려고 합니다. 
+문자열 배열 s1과 s2가 주어질 때 같은 원소의 개수를 return하도록 solution 함수를 완성해주세요.
+
+제한사항
+1 ≤ s1, s2의 길이 ≤ 100
+1 ≤ s1, s2의 원소의 길이 ≤ 10
+s1과 s2의 원소는 알파벳 소문자로만 이루어져 있습니다
+s1과 s2는 각각 중복된 원소를 갖지 않습니다.
+
+입출력 예
+s1	                s2	                            result
+["a", "b", "c"]	    ["com", "b", "d", "p", "c"]	    2
+["n", "omg"]	    ["m", "dot"]	                0
+ */
+
+import java.util.*;
+
+class Solution {
+    public int solution(String[] s1, String[] s2) {
+        int answer = 0;
+        List<String> s2List = new ArrayList<> (Arrays.asList(s2));
+        
+        for(int i = 0; i < s1.length; i++){
+            if(s2List.contains(s1[i])){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
