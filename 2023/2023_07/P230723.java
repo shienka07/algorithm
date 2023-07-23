83d0fc6b0a3fd4a4f72793fc8d58c1da79ac5ea4
JO KYEONGHYE <shienka07.ko@gmail.com>
2023-07-23 23:39:17 +0900

    P_문자열 겹쳐쓰기

---
+++ 2023/2023_07/P230723.java
@@ -0,0 +1,37 @@
+/*
+문자열 겹쳐쓰기
+https://school.programmers.co.kr/learn/courses/30/lessons/181943
+
+
+문제 설명
+문자열 my_string, overwrite_string과 정수 s가 주어집니다. 
+문자열 my_string의 인덱스 s부터 overwrite_string의 길이만큼을 
+문자열 overwrite_string으로 바꾼 문자열을 return 하는 solution 함수를 작성해 주세요.
+
+제한사항
+my_string와 overwrite_string은 숫자와 알파벳으로 이루어져 있습니다.
+1 ≤ overwrite_string의 길이 ≤ my_string의 길이 ≤ 1,000
+0 ≤ s ≤ my_string의 길이 - overwrite_string의 길이
+
+입출력 예
+my_string	         overwrite_string	s	result
+"He11oWor1d"	    "lloWorl"	        2	"HelloWorld"
+"Program29b8UYP"	"merS123"	        7	"ProgrammerS123"
+ */
+
+public class P230723 {
+    class Solution {
+        public String solution(String my_string, String overwrite_string, int s) {
+            String answer = "";
+            int ovwLen = overwrite_string.length();
+            int myLen = my_string.length();     
+            
+            answer += my_string.substring(0, s);
+            answer += overwrite_string;
+            if(answer.length() != myLen){
+                answer += my_string.substring(s+ovwLen, myLen);
+            }
+            return answer;
+        }
+    }
+}
