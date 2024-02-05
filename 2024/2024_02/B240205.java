fba6f73ef492f4f43581c9910187aa5ed8d21df1
JO KYEONGHYE <shienka07.ko@gmail.com>
2024-02-05 16:52:25 +0900

    B_1764번 듣보잡

---
+++ 2024/2024_02/B240205.java
@@ -0,0 +1,71 @@
+/*
+1764번 듣보잡
+https://www.acmicpc.net/problem/1764
+
+
+김진영이 듣도 못한 사람의 명단과, 보도 못한 사람의 명단이 주어질 때, 듣도 보도 못한 사람의 명단을 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 듣도 못한 사람의 수 N, 보도 못한 사람의 수 M이 주어진다. 
+이어서 둘째 줄부터 N개의 줄에 걸쳐 듣도 못한 사람의 이름과, N+2째 줄부터 보도 못한 사람의 이름이 순서대로 주어진다. 
+이름은 띄어쓰기 없이 알파벳 소문자로만 이루어지며, 그 길이는 20 이하이다. N, M은 500,000 이하의 자연수이다.
+듣도 못한 사람의 명단에는 중복되는 이름이 없으며, 보도 못한 사람의 명단도 마찬가지이다.
+
+출력
+듣보잡의 수와 그 명단을 사전순으로 출력한다.
+
+예제 입력 1 
+3 4
+ohhenrie
+charlie
+baesangwook
+obama
+baesangwook
+ohhenrie
+clinton
+예제 출력 1 
+2
+baesangwook
+ohhenrie
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class B240205 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        HashSet<String> unheard = new HashSet<String>();
+
+        for(int i = 0; i < n; i++){
+            unheard.add(br.readLine());
+        }
+
+        List<String> answer = new ArrayList<>();
+
+        for(int i = 0; i < m; i++){
+            String temp = br.readLine();
+            if(unheard.contains(temp)){
+                    answer.add(temp);
+            }
+        }
+
+        Collections.sort(answer);
+
+        System.out.println(answer.size());
+        for(int i = 0; i < answer.size(); i++){
+            System.out.println(answer.get(i));
+        }
+    }
+}
