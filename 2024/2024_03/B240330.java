dcc469ee673ced1250e79c31fb21b36a591c9d8a
JO KYEONGHYE <shienka07.ko@gmail.com>
2024-03-30 23:36:02 +0900

    B_16204번 카드 뽑기

---
+++ 2024/2024_03/B240330.java
@@ -0,0 +1,57 @@
+/*
+16204번 카드 뽑기
+https://www.acmicpc.net/problem/16204
+
+
+앞 면에 O와 X가 적혀있는 카드 N개가 있다. 
+N개의 카드 중 M개의 카드의 앞면에는 O가 한 개 적혀있고, 나머지 N-M개의 카드의 앞면에는 X가 한 개 적혀있다. 
+카드의 뒷 면은 두 종류의 카드 모두 같은 모양이라 구분할 수 없다.
+카드의 뒷 면에 O나 X를 하나씩 적으려고 한다. 이 때, O는 K개, X는 N-K개 적으려고 한다.
+앞 면과 뒷 면에 같은 모양이 적혀있는 카드의 최대 개수를 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 N, M, K가 주어진다. (1 ≤ N ≤ 1,000,000, 0 ≤ M, K ≤ N)
+
+출력
+첫째 줄에 앞 면과 뒷 면에 같은 모양이 적혀있는 카드의 최대 개수를 출력한다.
+
+예제 입력 1 
+4 3 2
+예제 출력 1 
+3
+
+예제 입력 2 
+10 0 10
+예제 출력 2 
+0
+
+예제 입력 3 
+5 3 3
+예제 출력 3 
+5
+
+예제 입력 4 
+7 5 2
+예제 출력 4 
+4
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class B240330 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        
+        int result = Math.min(m, k) + Math.min((n - m), (n - k));
+
+        System.out.println(result);
+    }
+}
+
