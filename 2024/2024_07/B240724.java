ef6fa488e70fbe3ec4e6136fd04174fc7d307c2f
JO KYEONGHYE <shienka07.ko@gmail.com>
2024-07-25 00:00:00 +0900

    B_별 찍기 - 1

---
+++ 2024/2024_07/B240724.java
@@ -0,0 +1,39 @@
+/*
+2438번 별 찍기 - 1
+https://www.acmicpc.net/problem/2438
+
+첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+
+입력
+첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
+
+출력
+첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다.
+
+예제 입력 1 
+5
+예제 출력 1 
+*
+**
+***
+****
+*****
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B240724 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= n; i++) {
+            for(int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
