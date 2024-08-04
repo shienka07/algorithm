5ee80acf53cc2ad9026d4adfa054dba1817b780f
JO KYEONGHYE <shienka07.ko@gmail.com>
2024-08-04 22:31:48 +0900

    B_11942번 고려대는 사랑입니다

---
+++ 2024/2024_08/B240804.java
@@ -0,0 +1,24 @@
+/*
+11942 고려대는 사랑입니다
+https://www.acmicpc.net/problem/11942
+
+
+고려대학교는 사랑입니다.
+
+출력
+첫째 줄에 “고려대학교”를 출력하세요.
+
+예제 입력 1 
+예제 출력 1 
+고려대학교
+*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B240804 {
+    public static void main(String[] args) throws IOException {
+        System.out.println("고려대학교");
+    }
+}
