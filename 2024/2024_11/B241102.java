e828ef14a1889c6eafb93d6cc16ffc7783d5a937
JO KYEONGHYE <shienka07.ko@gmail.com>
2024-11-03 23:27:56 +0900

    B_14215번 세 막대(수정)

--- 2024/2024_11/B241102.java
+++ 2024/2024_11/B241102.java
@@ -45,7 +45,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class B231102 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
