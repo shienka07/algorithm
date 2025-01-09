53135323b5bd0be86f9dc804a16b70fbcdd52505
JO KYEONGHYE <shienka07.ko@gmail.com>
2025-01-09 23:46:30 +0900

    P_340207번 [PCCE 기출문제] 1번 / 문자 출력

---
+++ 2025/2025_01/P250109.java
@@ -0,0 +1,26 @@
+/*
+340207번 [PCCE 기출문제] 1번 / 문자 출력
+https://school.programmers.co.kr/learn/courses/30/lessons/340207
+
+
+주어진 코드는 변수에 데이터를 저장하고 출력하는 코드입니다. 
+아래와 같이 출력되도록 빈칸을 채워 코드를 완성해 주세요.
+
+출력 예시
+3
+2
+1
+Let's go!
+*/
+    
+
+import java.util.Scanner;
+
+public class P250109 {
+    public static void main(String[] args) {
+        String message = "Let's go!";
+
+        System.out.println("3\n2\n1");
+        System.out.println(message);
+    }
+}
